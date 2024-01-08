2004e9c20c38eead848cec78217586dfd8e2e961
DESKTOP-ROJ80GD\LG <dhksl823@gmail.com>
2024-01-08 13:40:53 +0900

    ⚡️ 초기화버튼, date/month 관리 및 하위 컴포넌트 연결

--- src/components/main/BtnWrap.js
+++ src/components/main/BtnWrap.js
@@ -1,10 +1,12 @@
 import { FaRegCalendarAlt } from "react-icons/fa";
 import { MdOutlineRefresh } from "react-icons/md";
 
-export default function BtnWrap() {
+export default function BtnWrap({handleRefreshCalendar}) {
   return (
     <div className="flex  items-center text-[#666]">
-      <div className="w-[30px] h-[30px] rounded-[6px] shadow-lg bg-[#9f9f9f] text-[#fff] flex-center cursor-pointer mr-[4px]">
+      <div className="w-[30px] h-[30px] rounded-[6px] shadow-lg bg-[#9f9f9f] text-[#fff] flex-center cursor-pointer mr-[4px]" 
+        onClick={()=>handleRefreshCalendar()}
+      >
         <MdOutlineRefresh />
       </div>
       <div className="w-[30px] h-[30px] rounded-[6px] shadow-lg bg-[#9f9f9f] text-[#fff] flex-center cursor-pointer">
