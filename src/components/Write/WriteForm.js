f5e3e3af02918e403165a3d4447e0f6c1c39c94b
DESKTOP-ROJ80GD\LG <dhksl823@gmail.com>
2024-01-09 19:09:46 +0900

    ⚡️ db 삭제,수정 코드 개선

--- src/components/Write/WriteForm.js
+++ src/components/Write/WriteForm.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import SelectLevel from './SelectLevel';
 
-export default function WriteForm({ selectMonth, todayDate, setViewSection }) {
+export default function WriteForm({ selectMonth, todayDate, setViewSection, getData }) {
   const [level, setLevel] = useState('easy');
+  const [title, setTitle] = useState('');
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
+        getData(title);
+        setViewSection((prev) => !prev);
       }}
       className="py-[20px] px-[10px] flex flex-col text-sm"
     >
@@ -20,6 +23,7 @@ export default function WriteForm({ selectMonth, todayDate, setViewSection }) {
           type="text"
           placeholder="일정을 입력해주세요."
           className="w-full mt-[4px] point-border p-[8px] rounded-[4px] placeholder:text-[#dfdfdf] outline-0"
+          onChange={(e) => setTitle(e.target.value)}
         />
       </div>
       <button type="submit" className="bg-red-300 text-[#fff] rounded-[4px] py-[10px] mt-[20px] font-bold">
--- src/components/Write/WriteWrap.js
+++ src/components/Write/WriteWrap.js
@@ -2,7 +2,7 @@
 import CardToday from '../Calendar/CardToday';
 import LinkBtn from '../main/LinkBtn';
 import SelectLevel from './SelectLevel';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import WriteForm from './WriteForm';
 
 export default function WriteWrap({
@@ -14,6 +14,41 @@ export default function WriteWrap({
   selectMonth,
   setSelectMonth,
 }) {
+  const getData = async (content) => {
+    console.log(content);
+    try {
+      const data = await fetch(`http://localhost:9999/todolist`);
+      const response = await data.json();
+
+      const filteredData = response.find((item) => item.month === selectMonth && item.date === todayDate);
+      console.log(filteredData);
+
+      if (!filteredData) {
+        // 데이터가 없는경우 post요청으로 생성
+        const writeData = await fetch('http://localhost:9999/todolist', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            year: 24,
+            month: selectMonth,
+            date: todayDate,
+            content: [content],
+            success: false,
+          }),
+        });
+        console.log(writeData);
+      } else {
+        // 기존에 데이터가 있는경우 put으로 데이터 추가
+        console.log(filteredData);
+        return;
+      }
+    } catch (error) {
+      console.error('Data Fetching Error : ', error);
+    }
+  };
+
   return (
     <div className="w-[calc(100%-10px)] mx-auto">
       <div className="mt-[10px]">
@@ -21,7 +56,7 @@ export default function WriteWrap({
       </div>
       <div className="bg-[#fff] w-full min-h-[400px] max-h-[400px] overflow-y-auto mx-auto rounded-[10px] relative shadow-lg mt-[4px] solid-border">
         <CardToday month={selectMonth} day={todayDate} />
-        <WriteForm month={selectMonth} day={todayDate} setViewSection={setViewSection} />
+        <WriteForm month={selectMonth} day={todayDate} setViewSection={setViewSection} getData={getData} />
       </div>
     </div>
   );
